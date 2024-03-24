feb8d76f059ef36863e45e5911f556d57832d9c1
Marcos Vinicius Otaviano Gonçalves <137358236+Undercity1914@users.noreply.github.com>
2024-03-24 15:45:25 -0300

    Change of ex06

---
+++ Lista3/Ex06/src/main/java/lista3/ex06/Classes/Accont.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista3.ex06.Classes;
+
+/**
+ *
+ * @author marco
+ */
+public class Accont {
+    
+}
